8b1415e05b64110ffeabbc9bb67454f3641773e3
cbrje <chri1423@gmail.com>
2024-11-20 14:33:43 +0000

    sync

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }: 
