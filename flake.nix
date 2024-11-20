a02c53d1c0adad2f6b3060dbdfaf80a7031eeae3
cbrje <chri1423@gmail.com>
2024-11-20 14:23:25 +0000

    sync

--- flake.nix
+++ flake.nix
@@ -7,24 +7,22 @@
 
   outputs = { self, nixpkgs }: {
 
-    packages = { system, ... }: 
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+    packages = { system, ... }: let
+      pkgs = import nixpkgs { inherit system; };
     in {
       default = pkgs.stdenv.mkDerivation {
         pname = "imperium";
         version = "0.1.3";
-      
 
       src = pkgs.fetchurl {
         url = "https://github.com/scipo-code/ordinator-imperium-cli/archive/refs/tags/v0.1.3.tar.gz";
-        sha = "0pks4f58chvjdchy6h3h41w8fnplg9mk19hwf4rbjfs64zvvd1qb";
+        sha256 = "0pks4f58chvjdchy6h3h41w8fnplg9mk19hwf4rbjfs64zvvd1qb";
       };
 
       installPhase = ''
         mkdir -p $out/bin
-        tar -xzf $src -C $out/bin
+        tar -xzf $src
+        cp imperium $out/bin        
         chmod +x $out/bin/imperium
       '';
       };
