247998060b67e31479d2cf88949896df822a3d42
cbrje <chri1423@gmail.com>
2024-12-05 20:14:18 +0000

    sync

--- flake.nix
+++ flake.nix
@@ -17,12 +17,12 @@
             pname = "imperium";
             version = "0.1.10";
 
-          src = pkgs.fetchurl {
+          src1 = pkgs.fetchurl {
             url = "https://github.com/scipo-code/ordinator-imperium-cli/releases/download/v0.1.10/imperium.exe";
             sha256 = "164gasn0sg6z4wdnb1zw5hqd5zn49cp1hr1c4sir9ifyn9qv6wi5";
           };
 
-          src2= pkgs.fetchurl {
+          src= pkgs.fetchurl {
             url = "https://github.com/scipo-code/ordinator-imperium-cli/releases/download/v0.1.10/imperium";
             sha256 = "060b6ky1h2h8r5fjs0897lwqrql5j7ykkfq83fpjg57cijkyy3jk";
           };
